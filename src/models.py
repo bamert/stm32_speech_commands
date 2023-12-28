eb2b9f000ab858d29f785f61cfa77d4e40fd0a49
Nik Bamert <ndbamert@gmail.com>
2023-12-28 09:00:58 +0100

    add m4 model

--- src/models.py
+++ src/models.py
@@ -47,6 +47,42 @@ class M5(nn.Module):
         x = torch.flatten(x, 1)
         x = self.fc1(x)
         return F.log_softmax(x, dim=1)
+class M4(nn.Module):
+    """ 
+    Similar to M5, but not part of the paper.
+    Uses one layer less due to limited spatial context in M5.conv4 at 8000 input length (1sec 8hkz sample)
+    """
+    def __init__(self, n_input=1, n_output=35, stride=16, n_channel=32, use_pool3=True):
+        super().__init__()
+        self.conv1 = nn.Conv1d(n_input, n_channel, kernel_size=80, stride=stride)
+        self.bn1 = nn.BatchNorm1d(n_channel)
+        self.pool1 = nn.MaxPool1d(4)
+        self.conv2 = nn.Conv1d(n_channel, n_channel, kernel_size=3)
+        self.bn2 = nn.BatchNorm1d(n_channel)
+        self.pool2 = nn.MaxPool1d(4)
+        self.conv3 = nn.Conv1d(n_channel, 2 * n_channel, kernel_size=3)
+        self.bn3 = nn.BatchNorm1d(2 * n_channel)
+        self.pool3 = nn.MaxPool1d(4)
+        self.use_pool3 = use_pool3
+        self.global_avg_pool = nn.AdaptiveAvgPool1d(1)
+        self.fc1 = nn.Linear(2 * n_channel, n_output)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(self.bn1(x))
+        x = self.pool1(x)
+        x = self.conv2(x)
+        x = F.relu(self.bn2(x))
+        x = self.pool2(x)
+        x = self.conv3(x)
+        x = F.relu(self.bn3(x))
+        if self.use_pool3:
+            x = self.pool3(x)
+        x = self.global_avg_pool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        return F.log_softmax(x, dim=1)
+
 
 
 class AudioClassifier(pl.LightningModule):
@@ -54,7 +90,7 @@ class AudioClassifier(pl.LightningModule):
         super().__init__()
         self.val_correct_outputs = 0
         self.val_total_outputs = 0
-        self.model = M5(n_input=1, n_output=num_labels)
+        self.model = M4(n_input=1, n_output=num_labels, use_pool3=True)
 
     def forward(self, x):
         return self.model(x)
